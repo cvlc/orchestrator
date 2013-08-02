6d72c72e16a9232a795ce38c61f18dab918505a8
cvlc <calum.lacroix@gmail.com>
2013-08-03 00:35:01 +0100

    Adding comments to scripts - #1

--- testaddr.sh
+++ testaddr.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# This script can be run on the Docker host to check container IP addresses.
 for link in /var/run/netns/*; do
 longid=`echo $link | cut -d'/' -f5`
 sudo ip netns exec $longid ip addr show dev eth1
