d913b4fc4ed020662d9b740f56075b12c04746b8
cvlc <calum.lacroix@gmail.com>
2013-08-03 00:51:46 +0100

    Update vm.sh

--- dnsmasq/vm.sh
+++ dnsmasq/vm.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
-mac=$(dd bs=1 count=5 if=/dev/urandom 2>/dev/null | hexdump -v -e '/1 ":%02X"')
-mac="52$mac"
-echo $mac
-qemu-system-x86_64 -enable-kvm -hda debian.qcow2 -snapshot -net nic,macaddr=$mac -net bridge,br=br1 2>&1>/dev/null &
+# This will generate a random mac address and boot a named QEMU disk image (you must create or obtain one).
+# Changes to the VM image are not saved. 
+BRIDGE=br1
+IMAGE=debian.qcow2
+MAC=$(dd bs=1 count=5 if=/dev/urandom 2>/dev/null | hexdump -v -e '/1 ":%02X"')
+MAC="52$MAC"
+echo $MAC
+qemu-system-x86_64 -enable-kvm -hda $IMAGE -snapshot -net nic,macaddr=$MAC -net bridge,br=$BRIDGE 2>&1>/dev/null &
