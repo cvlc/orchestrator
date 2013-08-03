2bb3f59177bad2fd7b45fd66d023fff36a54dbf0
cvlc <calum.lacroix@gmail.com>
2013-08-03 01:12:43 +0100

    Update test_read.sh

--- test_read.sh
+++ test_read.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 # This script is useful for quickly listing all the components that Orchestrator is keeping track of.
+ADDRESS="127.0.0.1"
+PORT="8998"
+
 echo "Instance list:"
-INSTANCE_LIST=$(curl -s -k https://127.0.0.1:8998/instance 2>/dev/null)
+INSTANCE_LIST=$(curl -s -k "https://$ADDRESS:$PORT/instance" 2>/dev/null)
 echo $INSTANCE_LIST
 echo "-------"
 echo "Client list:"
-CLIENT_LIST=$(curl -s -k https://127.0.0.1:8998/client 2>/dev/null)
+CLIENT_LIST=$(curl -s -k "https://$ADDRESS:$PORT/client" 2>/dev/null)
 echo $CLIENT_LIST
 echo "-------"
 echo "Cloud list:"
-SERVICE_LIST=$(curl -s -k https://127.0.0.1:8998/cloud 2>/dev/null)
+SERVICE_LIST=$(curl -s -k "https://$ADDRESS:$PORT/cloud" 2>/dev/null)
 echo $SERVICE_LIST
 echo "-------"
 echo "DHCP list:"
-SERVICE2_LIST=$(curl -s -k https://127.0.0.1:8998/dhcp 2>/dev/null)
+SERVICE2_LIST=$(curl -s -k "ttps://$ADDRESS:$PORT/dhcp" 2>/dev/null)
 echo $SERVICE2_LIST
 echo "-------"
