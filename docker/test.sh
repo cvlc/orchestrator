ac292912d016b1c4ff96d741330ef5963161ff01
cvlc <calum.lacroix@gmail.com>
2013-08-03 01:09:04 +0100

    Update test.sh

--- docker/test.sh
+++ docker/test.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+# This will test the plugin helper by starting an identified container (provide the container shortID as the argument)
+
+SECRET="abcdef"
+ADDRESS="127.0.0.1"
+PORT="8997"
+
 if [[ "$1" ]]; then 
 ID=$(docker inspect "$1" | grep 'ID' | cut -d'"' -f4)
 echo "ID: $ID"
-RESULT=$(/usr/bin/curl -s -k --data "container_id=$ID&secret=abcdef" "https://127.0.0.1:8997/container" 2>/dev/null)
+RESULT=$(/usr/bin/curl -s -k --data "container_id=$ID&secret=$SECRET" "https://$ADDRESS:$PORT/container" 2>/dev/null)
 echo "Result:"
 echo "$RESULT"
 else
